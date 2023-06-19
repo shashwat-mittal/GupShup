16f60a4e4997dc21aa2ad12ad881147c5a3e1d4d
Shashwat <shashwatmittal.india@gmail.com>
2023-06-19 22:47:41 +0530

    fetch user image and name in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import { signOut } from 'firebase/auth'
-import React from 'react'
+import React, { useContext } from 'react'
 import { auth } from '../firebase'
+import { AuthContext } from '../context/AuthContext'
 
 export const Navbar = () => {
+  const {currentUser}= useContext(AuthContext)
   return (
     <div className='navbar'>
       <span className='logo'>GupShup</span>
       <div className='user'>
-        <img src="https://avatars.githubusercontent.com/u/74846797" alt=""/>
-        <span>Shashwat</span>
+        <img src={currentUser.photoURL} alt=""/>
+        <span>{currentUser.displayName}</span>
         <button onClick={()=>signOut(auth)}>logout</button>
       </div>
     </div>
