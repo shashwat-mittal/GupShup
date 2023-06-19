a2122770e009d6c318e287a46ad75d7525d7d044
Shashwat <shashwatmittal.india@gmail.com>
2023-06-19 23:06:27 +0530

    implement search user in sidebar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {db} from "../firebase"
+import { collection, query, where,getDocs } from "firebase/firestore";
+
 
 export const Search = () => {
+  const [username, setUsername]=useState("")
+  const [user, setUser]=useState(null)
+  const [err, setErr]=useState(false)
+
+  const handleSearch = async () =>{
+    const q = query(
+      collection(db,"users"),
+      where("displayName","==",username)
+    );
+    
+    try{
+    const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+      setUser(doc.data())
+    });
+    }catch(err){
+      setErr(true);
+    }
+
+  }
+
+  const handleKey = e =>{
+    e.code === "Enter" && handleSearch();
+  }
+
   return (
     <div className='search'>
       <div className='searchForm'>
-        <input type='text' placeholder='Find a user'/>
+        <input type='text' placeholder='Find a user' onKeyDown={handleKey} onChange={e=>setUsername(e.target.value)}/>
       </div>
-      <div className='userChat'>
-        <img src='https://avatars.githubusercontent.com/u/74846797' alt=''/>
+      {err && <span>User not found!</span>}
+      {user && <div className='userChat'>
+        <img src={user.photoURL} alt=''/>
         <div className='userChatInfo'>
-          <span>Shashwat</span>
+          <span>{user.displayName}</span>
         </div>
-      </div>
+      </div>}
     </div>
   )
 }
